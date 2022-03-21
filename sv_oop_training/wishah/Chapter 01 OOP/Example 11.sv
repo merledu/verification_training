5e33ed2dcb939b52fdfa270402c384e04e3675ff
Wishah Naseer <66886746+Wishah456@users.noreply.github.com>
2022-03-21 15:09:30 +0500

    Example 10 and 11 Added

---
+++ sv_oop_training/wishah/Chapter 01 OOP/Example 11.sv
@@ -0,0 +1,51 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Company:        MICRO-ELECTRONICS RESEARCH LABORATORY                                               //
+//                                                                                                     //
+// Engineers:      Wishah Naseer - Verification	Engineer                                               //
+//                                                                                                     //
+// Additional contributions by:                                                                        //
+//                                                                                                     //
+// Create Date:    20-MARCH-2022                                                                       //
+// Design Name:    SV OOP Practice                                                                     //
+// Module Name:    Example 11.sv                                                                       //
+// Project Name:   SV OOP Training                                                                     //
+// Language:       SystemVerilog - UVM                                                                 //
+//                                                                                                     //
+// Description:                                                                                        //
+//     - This code helps to understand how to shallow copy when an object can refer to another object  //
+//       same objects.....                                                                             //
+//                                                                                                     //
+// Revision Date:                                                                                      //
+//                                                                                                     //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+module top();  
+  class Mouth;
+    int teeth;
+    function new(int teeth);
+      this.teeth = teeth;
+    endfunction
+  endclass : Mouth
+
+	class Animal;  
+    Mouth m_h;
+    int age;
+
+    function new(int teeth, age);
+      m_h = new(teeth);
+      this.age = age;
+    endfunction
+    
+  endclass : Animal
+
+  Animal a_h,b_h;              //Handles
+  //b_h is a new object with same properties of a_h
+  initial begin
+    a_h = new(.teeth(2),.age(5));         //construct animal object
+    b_h = new a_h;              //construct new object and copy the values from a_h
+    b_h.age = 9;                //change age property in new object
+    b_h.m_h.teeth =4;
+    $display(a_h.age,a_h.m_h.teeth, b_h.age, b_h.m_h.teeth); //a_h and b_h refers to separate objects
+  end
+
+endmodule

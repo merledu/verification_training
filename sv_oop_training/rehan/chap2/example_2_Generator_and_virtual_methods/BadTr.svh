fab4f1f19be88cdff0740c33a4a251339e815248
Rehan Ejaz <rejaz@students.uit.edu>
2022-03-28 05:31:19 +0500

    example2 added

---
+++ sv_oop_training/rehan/chap2/example_2_Generator_and_virtual_methods/BadTr.svh
@@ -0,0 +1,26 @@
+class BadTr extends Transaction;
+  bit badCsm; // Corrupt the CSM 
+  // override existing Methods
+ 
+  function void calcCsm();
+    super.calcCsm();       // Compute checksum
+    if(badCsm) csm = ~csm; // Corrupt if bad
+  endfunction
+  
+  function void print();
+    $write("BadTr: badCsm=%b ", badCsm);
+    super.print();
+  endfunction
+
+	function void copy(BadTr rhs_h);
+    badCsm = rhs_h.badCsm;
+  endfunction
+
+  function Transaction clone();
+    BadTr b_h;
+    b_h = new();
+    clone = b_h;
+    clone.copy(this);
+  endfunction
+
+endclass

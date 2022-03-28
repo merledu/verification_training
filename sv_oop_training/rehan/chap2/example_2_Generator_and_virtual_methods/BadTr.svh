7f0dee6db5b5f2c3222e8631de6d953e18f39804
Rehan Ejaz <rejaz@students.uit.edu>
2022-03-28 13:59:41 +0500

    Bug removed

--- sv_oop_training/rehan/chap2/example_2_Generator_and_virtual_methods/BadTr.svh
+++ sv_oop_training/rehan/chap2/example_2_Generator_and_virtual_methods/BadTr.svh
@@ -2,21 +2,26 @@ class BadTr extends Transaction;
   bit badCsm; // Corrupt the CSM 
   // override existing Methods
  
-  function void calcCsm();
+  virtual function void calcCsm();
     super.calcCsm();       // Compute checksum
     if(badCsm) csm = ~csm; // Corrupt if bad
   endfunction
   
-  function void print();
+  virtual function void print();
     $write("BadTr: badCsm=%b ", badCsm);
     super.print();
   endfunction
 
-	function void copy(BadTr rhs_h);
-    badCsm = rhs_h.badCsm;
+	virtual function void copy(Transaction rhs_h);
+    BadTr b_h;
+    if(!$cast(b_h, rhs_h)) 
+    $fatal("cast error");
+    super.copy(rhs_h);
+    badCsm = b_h.badCsm;
   endfunction
 
-  function Transaction clone();
+
+  virtual function Transaction clone();
     BadTr b_h;
     b_h = new();
     clone = b_h;

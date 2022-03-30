a4db3c695e6d465a2ac972cffc87ffbc5b119501
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-03-30 12:19:39 +0500

    tx_env

---
+++ uvm_training/kinza/practice/CH2/example_1_random_transactions/env/tx_env.sv
@@ -0,0 +1,35 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Company:        MICRO-ELECTRONICS RESEARCH LABORATORY                                               //
+//                                                                                                     //
+// Engineers:      Kinza Qamar Zaman - Verification                                                    //
+//                                                                                                     //
+// Additional contributions by:                                                                        //
+//                                                                                                     //
+// Create Date:    17-MARCH-2022                                                                       //
+// Design Name:    Random transaction class                                                            //
+// Module Name:    tx_env.sv                                                                           //
+// Project Name:   tx_item generate random transaction for src and dst.                                //
+// Language:       SystemVerilog - UVM                                                                 //
+//                                                                                                     //
+// Description:                                                                                        //
+//          tx_env instantiate the agent in the build phase.                                           //
+//                                                                                                     //
+// Revision Date:                                                                                      //
+//                                                                                                     //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class tx_env extends uvm_env;
+	//Factory registration
+	`uvm_component_utils(tx_env)
+	//constructor
+	function new(string name,uvm_component parent);
+		super.new(name,parent);
+	endfunction 
+
+	tx_agent agt;
+	//building the components inside the hierarchy of environment class
+	virtual function void build_phase(uvm_phase phase);
+		agt = tx_agent::type_id::create("agt",this);
+	endfunction
+//Connect phase not required as we have no other component except of an agent class, exist inside the environment hierarchy
+endclass

646c17882e4633d4af4bd6a2a562b2c0912a325e
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-03-31 22:34:45 +0500

    Modified

--- uvm_training/kinza/practice/CH1/example_2_transaction_class/driver/tx_driver.sv
+++ uvm_training/kinza/practice/CH1/example_2_transaction_class/driver/tx_driver.sv
@@ -20,20 +20,23 @@
 class tx_driver extends uvm_driver #(tx_item); //Specialized class with tx_item
 	//Factory Registration
 	`uvm_component_utils(tx_driver)
+
 	/*constructor has 2 arguments: 
 	  1) name of the object
       2) handle to the parent */
 	function new(string name,uvm_component parent);
 		super.new(name,parent);
 	endfunction
+
 	virtual task run_phase(uvm_phase phase);
 		tx_item tx;
 		forever begin
 			seq_item_port.get_next_item(tx); //driver gets the tx_item through the blocking TLM port
-			transfer(tx); //Call transfer function
-			seq_item_port.item_done();	
+			transfer(tx);  					//Call transfer function
+			seq_item_port.item_done();		//tells the seqr, it is done with the item
 		end
 	endtask
+
 	virtual task transfer(tx_item tr);
 		`uvm_info("TRANSFER",$sformatf("tr.data=%3d",tr.data),UVM_LOW);
 	endtask

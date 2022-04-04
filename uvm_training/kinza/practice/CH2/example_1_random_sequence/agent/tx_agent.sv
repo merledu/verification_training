2a403cc649686e74776541d2a37af7b7c3d56252
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-04-04 11:33:50 +0500

    Modified

--- uvm_training/kinza/practice/CH2/example_1_random_sequence/agent/tx_agent.sv
+++ uvm_training/kinza/practice/CH2/example_1_random_sequence/agent/tx_agent.sv
@@ -22,20 +22,25 @@ class tx_agent extends uvm_agent;
 	//Factory registration
 	`uvm_component_utils(tx_agent)
 	//constructor
+
 	function new(string name,uvm_component parent);
 		super.new(name,parent);
 	endfunction
+
 	tx_driver drv;
 	//tx_monitor mon;
 	uvm_sequencer #(tx_item) sqr; //Never extended
+
 	//building the components inside the hierarchy of agent class
 	virtual function void build_phase(uvm_phase phase);
 		drv = tx_driver::type_id::create("drv",this);
 		sqr = new("sqr",this);
 	//	mon = tx_monitor::type_id::create("mon",this);
 	endfunction
+
 	//connectng the components inside the hierarchy of agent class
 	virtual function void connect_phase(uvm_phase phase);	
 		drv.seq_item_port.connect(sqr.seq_item_export);
 	endfunction
+	
 endclass

1e50bed634cdb7c7b12dd801c3fa9ce4741d22d1
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-04-06 22:45:54 +0500

    run.f

---
+++ uvm_training/kinza/practice/CH3/example_1_driver_sequencer_tlm_connection/compile_file/run.f
@@ -0,0 +1,19 @@
+/*-----------------------------------------------------------------
+File name     : run.f
+Developers    : Kinza Qamar Zaman
+Created       : 14-Mar-22
+Description   : Basic UVM transaction example 
+Notes         : From the Mentor "SystemVerilog UVM" training
+-------------------------------------------------------------------
+
+-----------------------------------------------------------------*/
+-64
+
+-uvmhome $UVMHOME
+
+// compile files
+
+
+//tx_sequence.sv tx_driver.sv tx_item.sv // compile transaction files
+top.sv          // compile top level module
+

83c0401684a4a254dd4653cde4c3cdb91488a837
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-04-07 23:47:49 +0500

    run.f

--- uvm_training/kinza/practice/CH3/example_1_driver_sequencer_tlm_connection/compile_file/run.f
+++ uvm_training/kinza/practice/CH3/example_1_driver_sequencer_tlm_connection/compile_file/run.f
@@ -1,12 +1,37 @@
-/*-----------------------------------------------------------------
-File name     : run.f
-Developers    : Kinza Qamar Zaman
-Created       : 14-Mar-22
-Description   : Basic UVM transaction example 
-Notes         : From the Mentor "SystemVerilog UVM" training
--------------------------------------------------------------------
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Company:        MICRO-ELECTRONICS RESEARCH LABORATORY                                               //
+//                                                                                                     //
+// Engineers:      Kinza Qamar Zaman - Verification                                                    //
+//                                                                                                     //
+// Additional contributions by:                                                                        //
+//                                                                                                     //
+// Create Date:    18-MARCH-2022                                                                       //
+// Design Name:    TLM connection                                                                      //
+// Module Name:    run.f                                                                               //
+// Project Name:   Driver-sequencer TLM connection                                                     //
+// Language:       SystemVerilog - UVM                                                                 //
+//                                                                                                     //
+// Description:                                                                                        //
+//         -  This file contain names of the files to be compiled, elaborate and simulate.             //
+// Revision Date:                                                                                      //
+//                                                                                                     //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
------------------------------------------------------------------*/
+-64
+
+-uvmhome $UVMHOME
+
+// compile files
+top.sv            // compile top level module
+
+//For simulating on Xcelium SimVision GUI mode, uncomment the below line
+//-access +rwc -gui
+
+//TEST_NAME
++UVM_TESTNAME=tx_test 
+
+//Seed randomization
+-svseed random
 -64
 
 -uvmhome $UVMHOME

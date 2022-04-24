9dda18a949a413a12f2b5349ca51f9b0d1e870cf
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-04-24 16:45:26 +0500

    tx_test

--- uvm_training/kinza/practice/CH1/example_2_transaction_class/test_pkg/tx_test.sv
+++ uvm_training/kinza/practice/CH1/example_2_transaction_class/test_pkg/tx_test.sv
@@ -14,26 +14,27 @@
 // Description:                                                                                      //
 //             The tx_test class is extended by uvm_test to start the tx_item transaction.           //
 // Revision Date:                                                                                    //
-//                                                                                                   //
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class tx_test extends uvm_test;
 
     //Factory registration
     `uvm_component_utils(tx_test)
+
     //constructor
     function new(string name,uvm_component parent);
         super.new(name,parent);
     endfunction
 
     tx_env env;
+    
     //building the components inside the hierarchy of environment class
     virtual function void build_phase(uvm_phase phase);
         env = tx_env::type_id::create("env",this); //creating env object
     endfunction
 
     /*Connect phase not required as we have no other component except of an agent class, exist inside 
-    the environment hierarchy */
+      the environment hierarchy */
     virtual task run_phase (uvm_phase phase);
         tx_sequence seq;
         seq = tx_sequence::type_id::create("seq");

e8af0205f0aee4c73596f99dd63ea6a526463bbc
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-04-24 16:37:49 +0500

    top

--- uvm_training/kinza/practice/CH1/example_1_transaction_driver_example/tb_top/top.sv
+++ uvm_training/kinza/practice/CH1/example_1_transaction_driver_example/tb_top/top.sv
@@ -12,21 +12,24 @@
 // Language:       SystemVerilog - UVM                                                                 //
 //                                                                                                     //
 // Description:                                                                                        //
-//             Top module is responsible to run tx_Test. We pass tx_test as a UVM_TESTNAME on          //
-// 			       the commmand line. run_test() gets the name of the test from the commandline        //
-//             and execute uvm_phases.		                                                           //
+//             Top module is responsible to run tx_test. We pass tx_test as a UVM_TESTNAME on          //
+// 			       the commmand line. run_test() gets the name of the test from the commandline            //
+//             and execute uvm_phases.		                                                             //
 // Revision Date:                                                                                      //
 //                                                                                                     //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 module top;
+
 	`include "uvm_macros.svh" //Includes uvm macros utility
-	import uvm_pkg::*; //import uvm base classes
+	import uvm_pkg::*; 			 //import uvm base classes
 	`include "tx_item.sv"
 	`include "tx_sequence.sv"
 	`include "tx_driver.sv"
+
 	/*initial begin
 		run_test(); // run_test Start execution of uvm phases
 	end */
+
 endmodule 
 

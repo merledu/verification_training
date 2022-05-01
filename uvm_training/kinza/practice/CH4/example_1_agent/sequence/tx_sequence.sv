e45da678d87c6c4ec6b18177c5563ceeb2ded907
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-05-01 12:38:53 +0500

    tx_sequence

--- uvm_training/kinza/practice/CH4/example_1_agent/sequence/tx_sequence.sv
+++ uvm_training/kinza/practice/CH4/example_1_agent/sequence/tx_sequence.sv
@@ -1,3 +1,21 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Company:        MICRO-ELECTRONICS RESEARCH LABORATORY                                               //
+//                                                                                                     //
+// Engineers:      Kinza Qamar Zaman - Verification                                                    //
+//                                                                                                     //
+// Additional contributions by:                                                                        //
+//                                                                                                     //
+// Create Date:    20-MARCH-2022                                                                       //
+// Design Name:    Analysis port example                                                               //
+// Module Name:    tx_sequence.sv                                                                      //
+// Project Name:   Agent-Monitor TLM connection              		                                       //
+// Language:       SystemVerilog - UVM                                                                 //
+//                                                                                                     //
+// Description:                                                                                        //
+//          tx_sequence randomizes the sequence items.                                                 //
+// Revision Date:                                                                                      //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 class tx_sequence extends uvm_sequence #(tx_item);
 	//Factory Registration
 	`uvm_object_utils(tx_sequence)
@@ -11,10 +29,13 @@ class tx_sequence extends uvm_sequence #(tx_item);
 		tx_item tx;
 		repeat(19) begin
 			tx = tx_item::type_id::create("tx"); //Body task creates transaction using factory creation
-			start_item(tx);		                  //start item. sequence body() blocks waiting for driver to be ready. Driver ask about sending transaction in its run 																phase
-			if (!tx.randomize())		           // Randomize transaction
+			start_item(tx);		                  /* start item. sequence body() blocks waiting for driver to be ready.
+																						 Driver ask about sending transaction in its run phase. */
+			if (!tx.randomize())		           //  Randomize transaction
 			  `uvm_fatal("Fatal","Randomization Failed");
-			finish_item(tx);		             //Sends transaction and waits for response from driver to know when it is ready again to generate and send transactions again. Sequence body() will be blocked again
+			finish_item(tx);		             /*		 Sends transaction and waits for response from driver to know when it 
+																				 		 is ready again to generate and send transactions again. 
+																				 		 Sequence body() will be blocked again. */
 		end
 	endtask
 	

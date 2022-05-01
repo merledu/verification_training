4b7df10256166980342308f90fe2f6dd70eabc74
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-05-01 12:31:54 +0500

    agent_config

--- uvm_training/kinza/practice/CH4/example_1_agent/config_object/agent_config.sv
+++ uvm_training/kinza/practice/CH4/example_1_agent/config_object/agent_config.sv
@@ -1,10 +1,30 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Company:        MICRO-ELECTRONICS RESEARCH LABORATORY                                               //
+//                                                                                                     //
+// Engineers:      Kinza Qamar Zaman - Verification                                                    //
+//                                                                                                     //
+// Additional contributions by:                                                                        //
+//                                                                                                     //
+// Create Date:    20-MARCH-2022                                                                       //
+// Design Name:    Analysis port example                                                               //
+// Module Name:    agent_config.sv                                                                     //
+// Project Name:   Agent-Monitor TLM connection              		                                       //
+// Language:       SystemVerilog - UVM                                                                 //
+//                                                                                                     //
+// Description:                                                                                        //
+//          agent_config object has configuration settings to build and configure the components.      //
+// Revision Date:                                                                                      //
+//                                                                                                     //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 class agent_config;
 
 	//virtual interface 
 	// virtual tb_ifc vif
 
-	//agent configuration]
+	//agent configuration
 	enum {UVM_ACTIVE, UVM_PASSIVE} active;
+
 	function new();
 		active = 0;
 	endfunction

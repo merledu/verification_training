9172bffc66f9005f095aafeddfc8bba4e32b39ae
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-05-01 12:48:47 +0500

    tx_item

--- uvm_training/kinza/practice/CH4/example_1_agent/sequence_item/tx_item.sv
+++ uvm_training/kinza/practice/CH4/example_1_agent/sequence_item/tx_item.sv
@@ -20,7 +20,7 @@
 class tx_item extends uvm_sequence_item;
 	//Factory Registration
 	`uvm_object_utils(tx_item) 	/* Sequencer or transactions are object classes so use object utility 
-								   macros to register it into the factory. */
+								  							 macros to register it into the factory. */
 	
 	//Constructor
 	function new (string name="tx_item");

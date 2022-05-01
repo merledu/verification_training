d5e3ce75c2a3e4cf9f4a11fafd3b4f3d6772a41f
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-05-01 12:32:30 +0500

    tx_driver

--- uvm_training/kinza/practice/CH4/example_1_agent/driver/tx_driver.sv
+++ uvm_training/kinza/practice/CH4/example_1_agent/driver/tx_driver.sv
@@ -1,3 +1,22 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Company:        MICRO-ELECTRONICS RESEARCH LABORATORY                                               //
+//                                                                                                     //
+// Engineers:      Kinza Qamar Zaman - Verification                                                    //
+//                                                                                                     //
+// Additional contributions by:                                                                        //
+//                                                                                                     //
+// Create Date:    20-MARCH-2022                                                                       //
+// Design Name:    Analysis port example                                                               //
+// Module Name:    tx_driver.sv                                                                        //
+// Project Name:   Agent-Monitor TLM connection              		                                       //
+// Language:       SystemVerilog - UVM                                                                 //
+//                                                                                                     //
+// Description:                                                                                        //
+//          tx_driver drives the configurations to the DUT via virtual interface.                      //
+// Revision Date:                                                                                      //
+//                                                                                                     //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 class tx_driver extends uvm_driver #(tx_item);
 	//Factory Registration
 	`uvm_component_utils(tx_driver)
@@ -9,6 +28,7 @@ class tx_driver extends uvm_driver #(tx_item);
 
 	//virtual tb_ifc vif;
 	//agent_cfg agt_cfg;
+
 	/*virtual function void build_phase (uvm_phase phase);
 		if(!uvm_config_db #(agent_cfg)::get(this,"","agt_cfg",agt_cfg))
 			`uvm_fatal("DRIVER","driver failed to get the virtual interface");
@@ -18,10 +38,12 @@ class tx_driver extends uvm_driver #(tx_item);
 	virtual task run_phase(uvm_phase phase);
 		tx_item tx;
 		forever begin
-			seq_item_port.get_next_item(tx);   //Driver call get_next_item which blocks the the driver until sequence send transaction handle to the driver by calling finish item. This action unblocks the sequence
+			seq_item_port.get_next_item(tx);  /*Driver call get_next_item which blocks the the driver until sequence send transaction 
+																					handle to the driver by calling finish item. This action unblocks the sequence. */
 			//vif.transfer(tx); 							//transfer the item to the dut via virtual interface
 			transfer(tx); 
-			seq_item_port.item_done(); 			//When the transaction completes, the driver calls item_done() to tell the seq it is done with the item. This call unblocks the sequence
+			seq_item_port.item_done(); 			 	/*When the transaction completes, the driver calls item_done() to tell the seq it is
+			 																		done with the item. This call unblocks the sequence. */
 		end
 	endtask
 
@@ -31,4 +53,5 @@ class tx_driver extends uvm_driver #(tx_item);
 	endtask
 endclass
 
-/*NOTE: This driver class is just a sample. The commented statements will be used once we have DUT. For now, simply ignore these statements.*/
+/*NOTE: This driver class is just a sample. 
+	The commented statements will be used once we have DUT. For now, simply ignore these statements.*/

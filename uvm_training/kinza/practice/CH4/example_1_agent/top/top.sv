16187dc92a450c50321387021b11f11ac4563263
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-05-01 12:41:15 +0500

    top

--- uvm_training/kinza/practice/CH4/example_1_agent/top/top.sv
+++ uvm_training/kinza/practice/CH4/example_1_agent/top/top.sv
@@ -1,3 +1,23 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Company:        MICRO-ELECTRONICS RESEARCH LABORATORY                                               //
+//                                                                                                     //
+// Engineers:      Kinza Qamar Zaman - Verification                                                    //
+//                                                                                                     //
+// Additional contributions by:                                                                        //
+//                                                                                                     //
+// Create Date:    20-MARCH-2022                                                                       //
+// Design Name:    Analysis port example                                                               //
+// Module Name:    top.sv                                                                              //
+// Project Name:   Agent-Monitor TLM connection              		                                       //
+// Language:       SystemVerilog - UVM                                                                 //
+//                                                                                                     //
+// Description:                                                                                        //
+//             Top module is responsible to run tx_Test. We pass tx_test as a UVM_TESTNAME on          //
+// 			   		 the commmand line. run_test() gets the name of the test from the commandline            //
+//             and execute uvm_phases.		                                                             //
+// Revision Date:                                                                                      //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 module top;
 
 	`include "uvm_macros.svh" //Includes uvm macros utility

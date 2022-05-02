48cc1bc85bba1c3b1012cdd7c864b4c21e890c2f
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-05-02 16:52:53 +0500

    tx_item

---
+++ uvm_training/kinza/practice/CH4/example_1_monitor_and_agent_analysis_port_example/sequence_item/tx_item.sv
@@ -0,0 +1,34 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Company:        MICRO-ELECTRONICS RESEARCH LABORATORY                                               //
+//                                                                                                     //
+// Engineers:      Kinza Qamar Zaman - Verification                                                    //
+//                                                                                                     //
+// Additional contributions by:                                                                        //
+//                                                                                                     //
+// Create Date:    20-MARCH-2022                                                                       //
+// Design Name:    Analysis port example                                                               //
+// Module Name:    tx_item.sv                                                                          //
+// Project Name:   Monitor and Agent analysis port example   		                                       //
+// Language:       SystemVerilog - UVM                                                                 //
+//                                                                                                     //
+// Description:                                                                                        //
+//         The tx_item class extends from uvm_sequence_item is used to generate 31-bit random a and    //
+//		     b items.                                                                             		   //
+// Revision Date:                                                                                      //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class tx_item extends uvm_sequence_item;
+	//Factory Registration
+	`uvm_object_utils(tx_item) 	/* Sequencer or transactions are object classes so use object utility 
+								  							 macros to register it into the factory. */
+	
+	//Constructor
+	function new (string name="tx_item");
+		super.new(name);
+	endfunction
+
+	rand bit [31:0] a;
+	rand bit [31:0] b;
+	
+endclass
+

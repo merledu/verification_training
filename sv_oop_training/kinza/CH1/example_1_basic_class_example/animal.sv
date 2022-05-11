35533b5aa86acf5829e29c15e82a9efc21d12f9a
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-05-11 15:38:05 +0500

    animal class

--- sv_oop_training/kinza/CH1/example_1_basic_class_example/animal.sv
+++ sv_oop_training/kinza/CH1/example_1_basic_class_example/animal.sv
@@ -12,27 +12,32 @@
 // Language:       SystemVerilog - OOP                                                                 //
 //                                                                                                     //
 // Description:                                                                                        //
-//         - This example class aims to demonstrate SystemVerilog Classes and how to utilize the class.//
-//                                                                                                     //
+//             This example class aims to demonstrate SystemVerilog Classes and how to utilize 				 //
+//             the class.                                                                              //
 // Revision Date:                                                                                      //
-//                                                                                                     //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class animal;
+
+	//Class properties
   int age;
   string name;
+
 //Class constructor
 	function new();
 		age = 0;
 		name = "";
 	endfunction 
+
 //Method to display properties of the animal
 	function void print();
 		$display("Animal name : %s and age : %0d",name,age);
 	endfunction
+
 //Method to get the age
 	task growOld (int how_long);
 		repeat(how_long)
 		#1 age++;
 	endtask
+
 endclass

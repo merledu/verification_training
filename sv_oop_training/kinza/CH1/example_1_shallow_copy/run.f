a83b503cc155804d73af0d951ea8d47bb35cbbf0
Kinza Qamar <kinzahqamarzaman@gmail.com>
2022-05-22 14:49:35 +0500

    run.f

---
+++ sv_oop_training/kinza/CH1/example_1_shallow_copy/run.f
@@ -0,0 +1,24 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Company:        MICRO-ELECTRONICS RESEARCH LABORATORY                                               //
+//                                                                                                     //
+// Engineers:      Kinza Qamar Zaman - Verification                                                    //
+//                                                                                                     //
+// Additional contributions by:                                                                        //
+//                                                                                                     //
+// Create Date:    15-MARCH-2022                                                                       //
+// Design Name:    Animal Class                                                                        //
+// Module Name:    run.f 		                                                                           //
+// Project Name:   Shallow copy Example                   					                                   //
+// Language:       SystemVerilog - OOP                                                                 //
+//                                                                                                     //
+// Description:                                                                                        //
+//             This example demonstrate the utilization of copy constructor.									 				 //
+// Revision Date:                                                                                      //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Files to be compile
+animal.sv 
+top.sv
+
+//For simulating on Xcelium SimVision GUI mode, uncomment the below line
+//-access +rwc -gui
